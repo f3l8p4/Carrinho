d9be72f019fca9483e25a194828286ead71cf32b
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-08-05 17:25:39 -0300

    Update mutations.js

--- Site/store/api/mutations.js
+++ Site/store/api/mutations.js
@@ -5,9 +5,4 @@ export default{
     STORECART(state,payload){
       state.cart = payload
     },
-    setData(state, payload) {
-        Object.keys(payload).forEach(k => {
-          state[k] = payload[k];
-        })
-      },
 }
