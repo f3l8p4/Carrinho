5a9ddde114f22d6daf821579851c900f0f1d38ea
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-08-09 03:24:05 -0300

    Update _id.vue

--- Site/pages/product/_id.vue
+++ Site/pages/product/_id.vue
@@ -6,6 +6,7 @@
         <h1>descricao do produto:{{this.$route.params.descricao}}</h1>
         <h1>preco do produto:{{this.$route.params.preco}}</h1>
         <h1>categoria do produto:{{this.$route.params.categoria}}</h1>
+                    <figure><img src="https://media.istockphoto.com/id/1384041600/pt/foto/table-or-chair-in-bedroom-studio-or-hotel-suite-room-with-wooden-modern-style-design-and.webp?s=612x612&w=is&k=20&c=mFGDy7P5DO2t8bnuoA7edXWt4aemRFCK-r6p2w1sYpo=" alt="produto"></figure>
     </div>
 </template>
 <script>
