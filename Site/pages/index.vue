064ec56afe13c68724c74144e52d8d0c078fcd70
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-08-09 03:23:39 -0300

    Update index.vue

--- Site/pages/index.vue
+++ Site/pages/index.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="bg-dark">
     <Header></Header>
-    <div v-for="produto in produtos" :key="produto.id" class="w-40 m-10">
-      <div class="card w-30 bg-base-100 shadow-xl">
-        <figure><img src="https://images.unsplash.com/photo-1657299156538-e08595d224ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"/></figure>
-        <div class="card-body">
-          <h2 class="fs-2 fw-bolder">{{produto.nome}}</h2>
-          <p class="fs-1">{{produto.descricao}}</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-success" @click="addCart(produto)">Add</button>
-            <button class="btn btn-sm btn-success" @click="page(produto)">Visualizar User</button>
+    <div class="mt-5">
+      <h3 class="text-4xl font-extralight text-white mb-4">Melhores produtos</h3>
+      <hr class="divide-y divide-dashed">
+      <div class="flex divide-x">
+        <div v-for="produto in produtos" :key="produto.id" class="max-w-sm mr-5 mt-6 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <img @click="page(produto)" class="p-8 cursor-pointer rounded-t-lg" src="https://media.istockphoto.com/id/1384041600/pt/foto/table-or-chair-in-bedroom-studio-or-hotel-suite-room-with-wooden-modern-style-design-and.webp?s=612x612&w=is&k=20&c=mFGDy7P5DO2t8bnuoA7edXWt4aemRFCK-r6p2w1sYpo=" alt="product image" />
+      <div class="px-5 pb-5 mb-6 d-inline-flex">
+              <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{produto.nome}}</h5>
+          <div class="flex justify-between items-center">
+              <span class="text-3xl font-bold text-gray-900 dark:text-white">R${{produto.preco}}</span>
+              <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
           </div>
-        </div>
       </div>
-    </div>
+      </div>
+      </div>
+  </div>
 
 </div>
 </template>
