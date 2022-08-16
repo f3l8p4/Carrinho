99fef2d172a7633f0001a2d369749c1a3e48f7ea
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-08-16 15:35:11 -0300

    Componentizando produtos

--- Site/components/AllProducts.vue
+++ Site/components/AllProducts.vue
@@ -1,10 +1,35 @@
 <template>
     <div>
-        
+        <h2 class="m-3 text-4xl text-white-50 fw-light">Produtos comprados</h2>
+        <hr>
+        <div class="overflow-x-auto mt-3">
+            <table class="table table-zebra w-full">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Produto</th>
+                  <th>Preço</th>
+                  <th>Descrição</th>
+                </tr>
+              </thead>
+              <tbody v-for="shop in shop" :key="shop.id">
+                <tr>
+                  <th>{{shop.id}}</th>
+                  <td>{{shop.nome}}</td>
+                  <td>{{shop.preco}}</td>
+                  <td>{{shop.descricao}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            shop:this.$store.state.shop.shop
+        }
+    },
 }
 </script>
--- Site/pages/pedidos.vue
+++ Site/pages/pedidos.vue
@@ -1,37 +1,12 @@
 <template>
     <div>
         <Header></Header>
-        <h2 class="m-3 text-4xl text-white-50 fw-light">Produtos comprados</h2>
-        <hr>
-        <div class="overflow-x-auto mt-3">
-            <table class="table table-zebra w-full">
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Produto</th>
-                  <th>Preço</th>
-                  <th>Descrição</th>
-                </tr>
-              </thead>
-              <tbody v-for="shop in shop" :key="shop.id">
-                <tr>
-                  <th>{{shop.id}}</th>
-                  <td>{{shop.nome}}</td>
-                  <td>{{shop.preco}}</td>
-                  <td>{{shop.descricao}}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+        <AllProducts></AllProducts>
     </div>
 </template>
 <script>
     import {mapGetters} from 'vuex'
     export default{
-        data(){
-            return{
-                shop:this.$store.state.shop.shop
-            }
-        },
+
     }
 </script>
