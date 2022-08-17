85da440459ff711a90c087a680c7ff9c1d5af1fa
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-08-17 00:17:08 -0300

    Update CardProduct.vue

--- Site/components/CardProduct.vue
+++ Site/components/CardProduct.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+            <div>
                 <img @click="page(produto)" class="p-8 cursor-pointer rounded-t-lg" src="" alt="product image"/>
                 <div class="px-5 pb-5 mb-6 d-inline-flex ">
                         <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">{{produto.nome}}</h5>
@@ -8,7 +8,7 @@
                         <span @click="addCart(produto)" class="text-white cursor-pointer bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</span>
                     </div>
                 </div>
-    </div>
+            </div>
 </template>
 <script>
     import {mapGetters} from 'vuex'
@@ -33,7 +33,10 @@
             }
         })
         }
+        },
+        mounted() {
+            console.log(this.produto)
+        },
 
-    },
     }
 </script>
