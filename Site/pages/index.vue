ed914ac34ef526cc786e6cc2bbc92d8603f5afa9
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-08-17 00:17:11 -0300

    Update index.vue

--- Site/pages/index.vue
+++ Site/pages/index.vue
@@ -5,7 +5,7 @@
       <h3 class="text-4xl font-extralight text-white mb-4">Melhores produtos</h3>
       <hr class="divide-y divide-dashed">
       <div class="flex  divide-x">
-        <CardProduct v-for="produto in produtos" :key="produto.id"></CardProduct>
+        <CardProduct :produto="produtos" v-for="produto in produtos" :key="produto.id"></CardProduct>
       </div>
   </div>
 
