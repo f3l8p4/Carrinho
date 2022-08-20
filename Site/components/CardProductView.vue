ab103bf215e1618a65aea6ddabfae42f6f929456
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2022-08-19 23:31:31 -0300

    Update CardProductView.vue

--- Site/components/CardProductView.vue
+++ Site/components/CardProductView.vue
@@ -3,10 +3,10 @@
         <div class="card card lg:card-side bg-base-200">
             <figure><img src="https://media.istockphoto.com/id/1384041600/pt/foto/table-or-chair-in-bedroom-studio-or-hotel-suite-room-with-wooden-modern-style-design-and.webp?s=612x612&w=is&k=20&c=mFGDy7P5DO2t8bnuoA7edXWt4aemRFCK-r6p2w1sYpo=" alt="produto"></figure>
             <div class="card-body">
-              <h1 class="text-3xl">{{this.$route.params.nome}}</h1>
-              <p class="text-xl mt-3">{{this.$route.params.descricao}}</p>
+              <h1 class="text-3xl">{{product.nome}}</h1>
+              <p class="text-xl mt-3">{{product.descricao}}</p>
               <div class="card-actions justify-end">
-                <h1 class="text-2xl">R${{this.$route.params.preco}}</h1>
+                <h1 class="text-2xl">R${{product.preco}}</h1>
                 <span @click="" class="btn-info">Add to cart</span>
                 <label for="modal" class="btn-info modal-button">Comprar Produto</label>
                 <ModalAdd></ModalAdd>
